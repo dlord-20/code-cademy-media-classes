30810ecec38db24db93e7d517f49ec854aececfc
Dlord <dthomaslord@gmail.com>
2021-08-18 11:42:15 +0300

    Completed project + added some extra features

--- main.js
+++ main.js
@@ -42,7 +42,11 @@ class Media {
     }
 
     addRating(newRating) {
-        this._ratings.push(newRating);
+        if(newRating > 0 && newRating < 6) {
+            this._ratings.push(newRating);
+        } else {
+            console.log('Invalid input: Number must be between 1 and 5');
+        }
     }
 
 }
@@ -69,6 +73,7 @@ class Movie extends Media {
         super(title);
         this._director = director;
         this._runtime = runTime;
+        this._movieCast = [];
     }
 
     get director() {
@@ -77,6 +82,107 @@ class Movie extends Media {
 
     get runTime() {
         return this._runtime;
+    }
+
+    get movieCast() {
+        return this._movieCast;
+    }
+
+    addMovieCast(castMember) {
+        this._movieCast.push(castMember);
+    }
+}
+
+class CD extends Media {
+    constructor(title, artist) {
+        super(title);
+        this._artist = artist;
+        this._songTitles = [];
+    }
+
+    get artist() {
+        return this._artist;
+    }
+
+    get songTitles() {
+        return this._songTitles;
+    }
+
+    addSongTitles(song) {
+        this._songTitles.push(song);
+    }
+
+    shuffle() {
+        let tempArray = this._songTitles;
+        let newOrder = [];
+        //Shuffles new array randomly
+        do {
+            let randNum = Math.floor(Math.random() * tempArray.length);
+            newOrder.push(tempArray[randNum]);
+            tempArray.splice(randNum, 1);
+        } while(tempArray.length > 0);
+
+        newOrder = this._songTitles.sh
+    }
+
+}
+
+class Catalog {
+    constructor() {
+        this._albums = [];
+        this._movies = [];
+        this._books = [];
+    }
+
+    get albums() {
+        return this._albums;
+    }
+
+    get movies() {
+        return this._movies;
+    }
+
+    get books() {
+        return this._books;
+    }
+
+    addAlbum(album) {
+        if(album instanceof CD) {
+            this._albums.push(album);
+        } else {
+            console.log('Incorrect input: Must be of class CD')
+        }
+    }
+
+    addMovie(movie) {
+        if(movie instanceof Movie) {
+            this._movies.push(movie);
+        } else {
+            console.log('Incorrect input: Must be of class Movie')
+        }
+    }
+
+    addBook(book) {
+        if(book instanceof Book) {
+            this._books.push(book);
+        } else {
+            console.log('Incorrect input: Must be of class Book')
+        }
+    }
+
+    print() {
+        console.log('Albums:')
+        for(let i = 0; i < this._albums.length; i++) {
+            console.log(`   ${i + 1}: ${this._albums[i].title}`)
+        }
+        console.log('Movies:')
+        for(let i = 0; i < this._movies.length; i++) {
+            console.log(`   ${i + 1}: ${this._movies[i].title}`)
+        }
+        console.log('Books:')
+        for(let i = 0; i < this._books.length; i++) {
+            console.log(`   ${i + 1}: ${this._books[i].title}`)
+        }
     }
 }
 
@@ -111,6 +217,19 @@ speed.addRating(1);
 speed.addRating(5);
 console.log(speed.getAverageRating());
 
+//Test CD
+const remains = new CD('Remains', '21 Pilots');
 
+remains.addSongTitles('Bam');
+remains.addSongTitles('Boom');
+remains.addSongTitles('Bang');
+remains.shuffle();
 
-
+const store = new Catalog();
+store.addAlbum('25');
+store.addAlbum(remains);
+store.addBook('25');
+store.addBook(historyOfEverything);
+store.addMovie('25');
+store.addMovie(speed);
+store.print();
